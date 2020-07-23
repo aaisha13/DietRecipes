b60314aed9f35997769bd0be71b784173aeb5470
aaishashaista <53037858+aaishashaista@users.noreply.github.com>
2020-07-24 00:58:46 +0530

    Initial Commit

---
+++ DietRecipes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DietRecipes
+//
+//  Created by Saeedahamad Sanglikar on 24/07/20.
+//  Copyright © 2020 Saeedahamad Sanglikar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
